dc71f6e15bc25c4002c6f7063f2a949c086c68b8
Ari Cohen <aricohen@usc.edu>
2020-04-28 14:37:51 -0700

    Update statsMethods.java
    update stats yay

--- src/Stats/statsMethods.java
+++ src/Stats/statsMethods.java
@@ -1,3 +1,5 @@
+package testStats;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -5,10 +7,193 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Types;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class statsMethods {
+public class StatsMethods {
 
-	
+	public static Boolean updateStats(int userToGameStatsID, int round, Boolean won)
+	{
+		//take userToGameStatsID to get userID and statsID
+		
+		Connection conn = null;
+		Statement st = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+
+		//should count wins, losses, and upcoming!
+		
+		int winUserGameStatsID = 0;
+		int winUserID = 0;
+		int winStatsID = 0;
+		int winCurElo = 0;
+		int winEloChange = 0;
+
+		int loseUserGameStatsID = 0;
+		int loseUserID = 0;
+		int loseStatsID = 0;
+		int loseCurElo = 0;
+		int loseEloChange = 0;
+		
+		//get system date and time
+		
+		
+		try {
+			conn = DriverManager.getConnection("jdbc:mysql://localhost/SportsWebsite?user=root&password=root");
+			st = conn.createStatement();
+
+			//get all statIDs corresponding to a user
+
+		    rs = st.executeQuery("SELECT * FROM UserToGameStats WHERE userToGameStatsID=" + userToGameStatsID);
+		    
+		    
+		    // get userID and statsID
+		    rs.next();
+		    
+		    if(won)
+		    {
+		    	winUserGameStatsID = userToGameStatsID;
+		    	winUserID = rs.getInt("userID");
+		    	winStatsID = rs.getInt("statsID");
+		    }
+		    else {
+		    	loseUserGameStatsID = userToGameStatsID;
+		    	loseUserID = rs.getInt("userID");
+		    	loseStatsID = rs.getInt("statsID");
+		    }
+		    
+
+		    
+			//use opponents table to get opponent's userToGameStatsID to get their userID and statsID
+		    rs = st.executeQuery("SELECT * FROM Opponents WHERE userToGameStatsID1 = " + userToGameStatsID
+		    		+ " OR userToGameStatsID2 = " + userToGameStatsID);
+		    
+		    rs.next();
+		    
+		    //spaghetti im sorry, only need to compare once for opponents
+		    if(rs.getInt("userToGameStatsID1") == userToGameStatsID) //ID2 is the other user's
+		    {
+		    	if(won)
+		    	{
+		    		loseUserGameStatsID = rs.getInt("userToGameStatsID2");
+		    	}
+		    	else {
+		    		winUserGameStatsID = rs.getInt("userToGameStatsID2");
+		    	}
+		    }
+		    else { //ID 1 is the other user's
+		    	if(won)
+		    	{
+		    		loseUserGameStatsID = rs.getInt("userToGameStatsID1");
+		    	}
+		    	else {
+		    		winUserGameStatsID = rs.getInt("userToGameStatsID1");
+		    	}
+		    }
+
+		    //use userToGameStatsID of other opponent to get their statsID and userID
+		    
+		    if(won)
+		    {
+			    rs = st.executeQuery("SELECT * FROM UserToGameStats WHERE userToGameStatsID= " + loseUserGameStatsID);
+			    rs.next();
+			    loseUserID = rs.getInt("userID");
+			    loseStatsID = rs.getInt("statsID");
+		    }
+		    else {
+			    rs = st.executeQuery("SELECT * FROM UserToGameStats WHERE userToGameStatsID= " + winUserGameStatsID);
+			    rs.next();
+			    winUserID = rs.getInt("userID");
+			    winStatsID = rs.getInt("statsID");		    
+			}
+		    
+		    
+		    System.out.println("winUserGameStatsID = " + winUserGameStatsID);
+		    System.out.println("loseUserGameStatsID = " + winUserGameStatsID);
+		    System.out.println("winUserID = " + winUserID);
+		    System.out.println("loseUserID = " + loseUserID);
+		    System.out.println("winStatsID = " + winStatsID);
+		    System.out.println("loseStatsID = " + loseStatsID);
+
+
+
+
+		    
+		    
+		    
+		    //then look up both userID's to get each player's current rank
+		    
+		    rs = st.executeQuery("SELECT userID, points FROM Users WHERE userID = " + winUserID
+		    		+ " OR userID = " + loseUserID);
+		    while(rs.next())
+		    {
+		    	if(winUserID == rs.getInt("userID"))
+		    	{
+		    		winCurElo = rs.getInt("points");
+		    	}
+		    	else {
+		    		loseCurElo = rs.getInt("points");
+		    	}
+		    }
+		    
+		    System.out.println("winCurElo = " + winCurElo);
+		    System.out.println("loseCurElo = " + loseCurElo);
+
+		    
+		    //get date in string
+		    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		    Date date = new Date();
+		    String curDate = dateFormat.format(date);
+		    System.out.println(curDate);
+		    
+		    int k = 50;
+		    
+		    //now we have ranks so must compute ELO
+		    Double probForWinUser = (1.0/(1.0 + Math.pow(10.0, (winCurElo - loseCurElo)/400.0)));
+			Double probForLoseUser = (1.0/(1.0 + Math.pow(10.0, (loseCurElo - winCurElo)/400.0)));
+			winEloChange = (int) Math.round(k*(1-probForWinUser));
+			loseEloChange = (int) Math.round(k*(0-probForLoseUser));
+			
+			System.out.println("winEloChange = " + winEloChange);
+			System.out.println("loseEloChange = " + loseEloChange);
+
+			
+			//now update the statsID table
+			st.executeUpdate("UPDATE Stats SET won = TRUE, bracketRound = " + round + 
+					", xp = " + winEloChange + ", gameDate = '" + curDate + 
+					"', oppRank = " + loseCurElo + " WHERE statsID = " + winStatsID);
+			
+			st.executeUpdate("UPDATE Stats SET won = FALSE, bracketRound = " + round + 
+					", xp = " + loseEloChange + ", gameDate = '" + curDate + 
+					"', oppRank = " + winCurElo + " WHERE statsID = " + loseStatsID);
+			
+			System.out.println("Got here 2");
+
+			//now just update ranks of both users
+			
+			st.executeUpdate("UPDATE Users "
+					+ "SET points = " + (winEloChange + winCurElo)
+					+ " WHERE UserID = " + winUserID);
+
+			System.out.println("Win userID = " + winUserID);
+			System.out.println("Lose userID = " + loseUserID);
+			
+			st.executeUpdate("UPDATE Users "
+					+ "SET points = " + (loseEloChange + loseCurElo)
+					+ " WHERE UserID = " + loseUserID);
+		    
+			return true;
+		}
+		catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+			return false;
+        }
+		finally {
+			
+			//close stuff I guess
+		}			
+	}
 	
 	public static int blankStatsRow()
 	{
@@ -44,15 +229,8 @@ public class statsMethods {
             System.out.println(ex.getMessage());
         }
 		finally {
-			
+			return 0;
 		}
-		return 0;
-	}
-	
-	public static void main(String[] args)
-	{
-		int id = blankStatsRow();
-		System.out.println(id);
 	}
 	
 }
