1cf9ee91fffe36462dc733b7d40922c1985cdbee
Gireesh Mahajan <gmahajan@usc.edu>
2020-04-29 16:15:20 -0700

    blap

---
+++ src/Friends/AddFriend.java
@@ -0,0 +1,66 @@
+package Friends;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import sql.JDBCBracketStuff;
+
+/**
+ * Servlet implementation class AddFriend
+ */
+@WebServlet("/AddFriend")
+public class AddFriend extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AddFriend() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session =request.getSession(false);
+		int userID = -1;
+		int gameType = -1;
+		
+		boolean success = true;
+		Object ui = session.getAttribute("userID");
+		String userid = null;
+		System.out.println("printing userid from session");
+		System.out.println((Integer)ui);
+		if (ui == null) {
+			success = false;
+		} else {
+			try {
+				userID = (Integer)ui;
+				userid = Integer.toString(userID);
+			} catch (NumberFormatException e) {
+				success = false;
+			}
+		}
+		
+		if (success) {
+			String friendUsername = request.getParameter("usernameToAdd");
+			int result = JDBCBracketStuff.addFriend(userID, friendUsername);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/tournaments/UpdateBracket.java
@@ -0,0 +1,73 @@
+package tournaments;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import sql.JDBCBracketStuff;
+
+/**
+ * Servlet implementation class UpdateBracket
+ */
+@WebServlet("/UpdateBracket")
+public class UpdateBracket extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdateBracket() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String slot1 = request.getParameter("slot1");
+		String slot2 = request.getParameter("slot2");
+		String bracketID1 = request.getParameter("bracketID");
+		String won = request.getParameter("won");
+		boolean success = true;
+		String error = null;
+		if (slot1 != null && slot2 != null && bracketID1 != null && won != null) {
+			int slotOne = -1;
+			int slotTwo = -1;
+			int bracketID = -1;
+			try {
+				slotOne = Integer.parseInt(slot1);
+				slotTwo = Integer.parseInt(slot2);
+				bracketID = Integer.parseInt(bracketID1);
+				success = true;
+				if (!won.equals("yes") && !won.equals("no")) {
+					success = false;
+					error = "please enter whether won is yes or no";
+				} 
+			} catch (NumberFormatException e) {
+				error = "please enter valid slot numbers and bracket IDs";
+			}
+			if (success) {
+				if (!JDBCBracketStuff.update(slotOne, slotTwo, won.equals("yes"), bracketID)) {
+					success = false;
+					error = "The backend sucks lolol";
+				}
+			}
+		}
+		if (success) {
+			
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
